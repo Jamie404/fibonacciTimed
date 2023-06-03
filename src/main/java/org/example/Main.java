a3a43c2bc3811d8d6197ed8b1ee61bbcd6e48f7d
Jamie404 <jamieleemcgee@hotmail.com>
2023-06-03 19:53:46 +0100

    Maven redo.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class Main {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
@@ -7,7 +9,6 @@ public class Main {
         for (int i = 0; i < N; i++) {
             System.out.print(fib(i) + " ");
         }
-
         long finish = System.currentTimeMillis();
         System.out.println(" ");
         long timeElapsed = finish - start;
@@ -15,9 +16,9 @@ public class Main {
     }
 
     private static int fib(int n) {
-        if (n <= 1)
+        if (n <= 1) {
             return n;
-
+        }
         return fib(n - 1) + fib(n - 2);
     }
 }
